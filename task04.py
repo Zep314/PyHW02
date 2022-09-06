f407fba84d1e9fa7b9f0f57efef68744a438fb10
Dmitry Mordanov <mordanov314@gmail.com>
2022-09-06 09:18:59 +0300

    final commit

--- task04.py
+++ task04.py
@@ -1,7 +1,6 @@
 # Задача 17. Задайте список из N элементов, заполненных числами из промежутка[-N, N].
 # Найдите произведение элементов на указанных позициях.
-# Позиции хранятся в файле file.txt в однойстроке одно число.
-
+# Позиции хранятся в файле file.txt в одной строке одно число.
 
 from random import randint
 
@@ -18,10 +17,12 @@ print(my_list)
 with open(my_filename,'r') as f:
     file_context =list(map(int,f.readlines())) # читаем в строку -
                                                # и каждую прочитанную строку обращаем в int
+                                               # и все это запихиваем в список
 print("Позиции элементов списка, которые надо перемножить:")
 print(file_context)
 
-list_to_multiple = [my_list[i] for i in file_context]
+list_to_multiple = [my_list[i] for i in file_context] # надо бы сделать проверку на выход за пределы списка
+                                                      # но надеемся, что в файле - корректные значения
 print("Список для перемножения:")
 print(list_to_multiple)
 res = 1
